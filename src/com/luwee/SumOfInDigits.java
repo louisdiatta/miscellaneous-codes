e5b5eb29b85473b237ae586994ab2a961fed39ec
Louis Omar DIATTA <46378861+louisdiatta@users.noreply.github.com>
2019-09-16 12:20:08 +0000

    Create SumOfInDigits.java

---
+++ src/com/luwee/SumOfInDigits.java
@@ -0,0 +1,7 @@
+public class SumOfInDigits {
+    public static void main(String[] args) {
+	int result, n = 452;
+	for (result = 0; n > 0; result += n % 10, n /= 10);
+	System.out.println(result);
+    }
+}
